f02a648c8b3a485c68f0c059e47c050b45b998bb
Oscar-Boj <oscar.boj2000@gmail.com>
2024-08-04 18:48:47 -0600

    first commit

---
+++ src/main/java/com/MiNombre/MiNombreApplication.java
@@ -0,0 +1,13 @@
+package com.MiNombre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiNombreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiNombreApplication.class, args);
+	}
+
+}
